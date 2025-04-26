cf75e9c759cc11aabc3abaa709ab0bf3667113f4
KyWariOro <steinson028@gmail.com>
2025-04-26 01:37:45 -0300

    Adapting files
    I've adapted my files to your current Pull to avoid conflicts

--- src/js/category.js
+++ src/js/category.js
@@ -1,17 +1,61 @@
 const params = new URLSearchParams(window.location.search);
 const info = params.get("data");
-//const urlParams = new URLSearchParams(window.location.search);
-//const current_category = params.get('data');
 document.getElementById("info").textContent = info;
 let list_of_records =document.getElementById("records");
 const back_btn=document.getElementById("back-btn");
 const add_cat_btn=document.getElementById("add-cat");
+const search_bar=document.getElementById("search-bar");
 
 let existing_records = JSON.parse(localStorage.getItem("Records")) || [];
+
+
+function filter_by_substring(substring,list){
+    let records_contain_substr=[]
+    if (list.length>0){
+        for (let i=0;i<list.length;i++){
+            let current_record=list[i]
+            let record_title=(current_record.title).toLowerCase()
+            if (record_title.includes(substring.toLowerCase())){
+                records_contain_substr.push(current_record)
+            }
+        }
+        return records_contain_substr
+    }
+}
+
+
+search_bar.addEventListener("keypress",function(event){
+    if (event.key=="Enter"){
+        let cat_records=get_list_records_by_cat();
+        text_to_search=search_bar.value;
+        filtered_records=filter_by_substring(text_to_search,cat_records);
+        get_records_by_cat(filtered_records);
+    }
+})
+
+
 
 add_cat_btn.addEventListener("click",function(){
     create_record(info);
 })
+
+
+
+function get_list_records_by_cat(){
+    let category_records=[]
+    if (existing_records.length>0){
+        for (let i=0;i<existing_records.length;i++){
+            if (existing_records[i].category == info ){
+                category_records.push(existing_records[i])
+            }
+        }
+        return category_records
+
+    }
+    return category_records
+}
+
+
 
 function shorten_text(text){
     if (text.length>64){
@@ -32,20 +76,25 @@ function check_if_record_isunique(title,url,category){
         }
     }
     return true;
+
+
 }
+
+
 
 function create_record(category) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         if (tabs.length > 0 && tabs[0].url) {
             let url = tabs[0].url;
-            let title = tabs[0].title;
+            let title = tabs[0].title || "Untitled Page";
             if (check_if_record_isunique(title,url,category)){
             let record = { url: url, category: category, title: title };
             existing_records.push(record);
 
             localStorage.setItem("Records", JSON.stringify(existing_records));
 
-            get_records_by_cat(category) //updating the UI need to be changed later
+            let cat_rec=get_list_records_by_cat()
+            get_records_by_cat(cat_rec) //updating the UI need to be changed later for performance
         } else {
             console.error("Tab or URL not available");
         }
@@ -56,20 +105,29 @@ function create_record(category) {
     });
 }
 
-function get_records_by_cat(cat){
+
+
+
+
+
+function get_records_by_cat(records){
     list_of_records.innerHTML = "";
-    category_records=[]
-    let all_records = JSON.parse(localStorage.getItem("Records")) || [];
-    if (all_records.length>0){
-    for (let i=0;i<all_records.length;i++){
-        if (all_records[i].category == cat ){
+    //category_records=[]
+    if (records.length>0){
+    for (let i=0;i<records.length;i++){
             //category_records.push(all_records[i])
-            record=all_records[i];
+            let record=records[i];
             let li_record=document.createElement("li");
             let record_link=document.createElement("a");
             record_link.href=record.url;
             record_link.innerText=shorten_text(record.title);
-            record_link.setAttribute("data-info",record.title)
+
+            record_link.addEventListener("click",function(e){
+                e.preventDefault();
+                chrome.tabs.create({url:record.url,active:false});
+            })
+            
+            record_link.setAttribute("data-info",record.title);
             if ((record_link.getAttribute("data-info")).length>64){
             record_link.addEventListener("mouseover",function(){
                 record_link.innerText=record_link.getAttribute("data-info");
@@ -83,8 +141,9 @@ function get_records_by_cat(cat){
             delete_btn.innerText="X"
             delete_btn.setAttribute("record-id",i);
             delete_btn.addEventListener("click",function(){
-                all_records.splice(i,1);
-                localStorage.setItem("Records", JSON.stringify(all_records));
+                let index=existing_records.indexOf(record)
+                existing_records.splice(index,1);
+                localStorage.setItem("Records", JSON.stringify(existing_records));
                 this.parentElement.remove(); //this stills need to be tested
 
             })
@@ -97,6 +156,5 @@ function get_records_by_cat(cat){
     }
 }
     //return category_records
-}
-
-get_records_by_cat(info)
+let cat_rec=get_list_records_by_cat();
+get_records_by_cat(cat_rec);
